d7adfc542befa3ea7a6ca20a7b417c9a7109640c
Vaibhav Vadgama <vaibhav.vadgama.it@gmail.com>
2025-06-05 00:50:31 -0400

    Poetry structure update

--- bigquery/reader.py
+++ bigquery/reader.py
@@ -1,5 +1,5 @@
 from google.cloud import bigquery
-from io_shared_library.config import settings
+from config import settings
 
 def read_from_bigquery(query: str):
     client = bigquery.Client(project=settings.PROJECT_ID)
